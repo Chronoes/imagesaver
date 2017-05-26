4888e86f8b0fc0f102d50218e504347e137cc406
Chronoes <martentarkin@gmail.com>
2017-05-26 17:44:03 +0300

    Changed server to CLI instead

--- image.go
+++ image.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+
 	"github.com/cenkalti/dominantcolor"
 	"github.com/disintegration/imaging"
 	"github.com/lucasb-eyer/go-colorful"
-	"image"
-	"image/color"
 )
 
 type imageInfo struct {
-	Source, Destination            string
-	AspectRatio, AspectMultipliers [2]int
+	Source, Destination               string
+	AspectRatio                       [2]int
+	heightMultiplier, widthMultiplier int
 }
 
 type aggregateColor func(image.Image) color.Color
@@ -25,7 +27,7 @@ func dominantColor(img image.Image) color.Color {
 }
 
 func rgbToColorful(r, g, b uint32) colorful.Color {
-	return colorful.Color{float64(r) / 0xffff, float64(g) / 0xffff, float64(b) / 0xffff}
+	return colorful.Color{R: float64(r) / 0xffff, G: float64(g) / 0xffff, B: float64(b) / 0xffff}
 }
 
 func prominentColor(img image.Image) color.Color {
@@ -55,79 +57,75 @@ func prominentColor(img image.Image) color.Color {
 	return colorful.Hcl(float64(maxIndex), chromas[maxIndex]/maxHue, luminances[maxIndex]/maxHue)
 }
 
-func calcEdgeLength(length int) int {
+func calculateEdgeLength(length int) int {
 	return int(float32(length) * 0.05)
 }
 
-func (i *imageInfo) calcMultipliers(width, height int) {
-	i.AspectMultipliers[0] = width / i.AspectRatio[0]
-	i.AspectMultipliers[1] = height / i.AspectRatio[1]
-}
-
-func (i *imageInfo) EdgeColors(baseImg image.Image, aggregate aggregateColor) (edge1, edge2 color.Color, ok bool) {
+func (i *imageInfo) AggregateEdgeColors(baseImg image.Image, aggregate aggregateColor) (firstEdge, secondEdge color.Color) {
 	imgBounds := baseImg.Bounds()
-	WOverflow := imgBounds.Dx() % i.AspectRatio[0]
-	HOverflow := imgBounds.Dy() % i.AspectRatio[1]
-	i.calcMultipliers(imgBounds.Dx(), imgBounds.Dy())
-	ok = false
-	if !(WOverflow == 0 && HOverflow == 0 && i.AspectMultipliers[0] == i.AspectMultipliers[1]) {
-		done := make(chan bool)
-		if i.AspectMultipliers[0] > i.AspectMultipliers[1] {
-			edgeLength := calcEdgeLength(imgBounds.Dy())
-			go func() {
-				edge1 = aggregate(imaging.CropAnchor(baseImg, imgBounds.Dx(), edgeLength, imaging.TopLeft))
-				done <- true
-			}()
-			edge2 = aggregate(imaging.CropAnchor(baseImg, imgBounds.Dx(), edgeLength, imaging.BottomLeft))
-		} else {
-			edgeLength := calcEdgeLength(imgBounds.Dx())
-			go func() {
-				edge1 = aggregate(imaging.CropAnchor(baseImg, edgeLength, imgBounds.Dy(), imaging.TopLeft))
-				done <- true
-			}()
-			edge2 = aggregate(imaging.CropAnchor(baseImg, edgeLength, imgBounds.Dy(), imaging.TopRight))
-		}
-		ok = <-done
+	done := make(chan bool)
+	if i.widthMultiplier > i.heightMultiplier {
+		edgeLength := calculateEdgeLength(imgBounds.Dy())
+		go func() {
+			firstEdge = aggregate(imaging.CropAnchor(baseImg, imgBounds.Dx(), edgeLength, imaging.TopLeft))
+			done <- true
+		}()
+		secondEdge = aggregate(imaging.CropAnchor(baseImg, imgBounds.Dx(), edgeLength, imaging.BottomLeft))
+	} else {
+		edgeLength := calculateEdgeLength(imgBounds.Dx())
+		go func() {
+			firstEdge = aggregate(imaging.CropAnchor(baseImg, edgeLength, imgBounds.Dy(), imaging.TopLeft))
+			done <- true
+		}()
+		secondEdge = aggregate(imaging.CropAnchor(baseImg, edgeLength, imgBounds.Dy(), imaging.TopRight))
 	}
+	<-done
 	return
 }
 
 func (i imageInfo) Normalise(img image.Image) *image.NRGBA {
-	return imaging.CropCenter(img, i.AspectRatio[0]*i.AspectMultipliers[0], i.AspectRatio[1]*i.AspectMultipliers[1])
+	return imaging.CropCenter(img, i.AspectRatio[0]*i.widthMultiplier, i.AspectRatio[1]*i.heightMultiplier)
 }
 
-func (i imageInfo) FillEdges(normed image.Image, edge1, edge2 color.Color) *image.NRGBA {
+func (i imageInfo) CreateSplitBackground(normed image.Image, firstEdge, secondEdge color.Color) *image.NRGBA {
 	width := i.AspectRatio[0]
 	height := i.AspectRatio[1]
-	position := new(image.Point)
-	if i.AspectMultipliers[0] > i.AspectMultipliers[1] {
-		width *= i.AspectMultipliers[0]
-		height *= i.AspectMultipliers[0]
+	var position image.Point
+	if i.widthMultiplier > i.heightMultiplier {
+		width *= i.widthMultiplier
+		height *= i.widthMultiplier
 		position.X = 0
 		position.Y = height / 2
 	} else {
-		width *= i.AspectMultipliers[1]
-		height *= i.AspectMultipliers[1]
+		width *= i.heightMultiplier
+		height *= i.heightMultiplier
 		position.X = width / 2
 		position.Y = 0
 	}
-	splitBackground := imaging.Paste(imaging.New(width, height, edge1), imaging.New(width, height, edge2), *position)
-	return imaging.PasteCenter(splitBackground, normed)
+	return imaging.Paste(imaging.New(width, height, firstEdge), imaging.New(width, height, secondEdge), position)
 }
 
 func (i imageInfo) CompileImage(aggregate aggregateColor) error {
 	img, err := imaging.Open(i.Source)
 	if err != nil {
-		return fmt.Errorf("Error opening file: %s; type: %v", i.Source, err)
+		return fmt.Errorf("Error opening file: %s; %v", i.Source, err)
 	}
-	if edge1, edge2, ok := i.EdgeColors(img, aggregate); ok {
-		normalised := i.FillEdges(i.Normalise(img), edge1, edge2)
-		err = imaging.Save(normalised, i.Destination)
+	imgBounds := img.Bounds()
+
+	var widthOverflow, heightOverflow int
+	i.widthMultiplier, widthOverflow = div(imgBounds.Dx(), i.AspectRatio[0])
+	i.heightMultiplier, heightOverflow = div(imgBounds.Dy(), i.AspectRatio[1])
+	if !(widthOverflow == 0 && heightOverflow == 0 && i.widthMultiplier == i.heightMultiplier) {
+		firstEdge, secondEdge := i.AggregateEdgeColors(img, aggregate)
+		normalised := i.Normalise(img)
+		splitBackground := i.CreateSplitBackground(normalised, firstEdge, secondEdge)
+		err = imaging.Save(imaging.PasteCenter(splitBackground, normalised), i.Destination)
 	} else {
 		err = imaging.Save(img, i.Destination)
 	}
+
 	if err != nil {
-		return fmt.Errorf("Error saving image to: %s; type: %v", i.Destination, err)
+		return fmt.Errorf("Error saving image to: %s; %v", i.Destination, err)
 	}
 	return nil
 }
--- main.go
+++ main.go
@@ -2,40 +2,67 @@ package main
 
 // TODO: Run tests for average, dominant and prominent aggregations for various images
 // TODO: Intelligent selection of aggregations for images with different backgrounds (based on above test results)
-// TODO: Separate the main package to make it independent of server, implement CLI
 
 import (
 	"encoding/json"
-	"fmt"
+	"io"
 	"log"
-	"net/http"
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
 )
 
-func dataHandler(rw http.ResponseWriter, req *http.Request) {
-	decoder := json.NewDecoder(req.Body)
-	var imgData imageInfo
-	err := decoder.Decode(&imgData)
-	if err != nil {
-		http.Error(rw, "Error decoding JSON", 400)
-		return
-	}
-	log.Printf("File recieved: %s\n", filepath.Base(imgData.Source))
+func div(base, modulo int) (int, int) {
+	return base / modulo, base % modulo
+}
+
+func processImage(imageData imageInfo) {
 	startTime := time.Now()
-	if err := imgData.CompileImage(averageColor); err != nil {
-		http.Error(rw, err.Error(), 400)
+	if err := imageData.CompileImage(averageColor); err != nil {
+		log.Print(err)
 		return
 	}
-	rw.Write([]byte(fmt.Sprintf("%s\n", imgData.Destination)))
 	timeTaken := time.Since(startTime)
-	log.Printf("File complete: %s time: %vs", filepath.Base(imgData.Source), timeTaken.Seconds())
+	log.Printf("File complete: %s time: %vs", filepath.Base(imageData.Source), timeTaken.Seconds())
+}
+
+func processImagesFromJSON(r io.Reader, concurrentProcessingLimit int) {
+	decoder := json.NewDecoder(r)
+	token, err := decoder.Token()
+	if err != nil {
+		log.Fatal(err)
+	} else if t, ok := token.(json.Delim); !ok || t != '[' {
+		log.Fatalln("Expected JSON to be an array")
+	}
+
+	var wg sync.WaitGroup
+	defer wg.Wait()
+
+	queue := make(chan imageInfo, concurrentProcessingLimit)
+	defer close(queue)
+
+	for decoder.More() {
+		var imageData imageInfo
+		if err = decoder.Decode(&imageData); err != nil {
+			log.Fatal(err)
+		}
+		wg.Add(1)
+		queue <- imageData
+		go func() {
+			processImage(imageData)
+			<-queue
+			wg.Done()
+		}()
+		log.Printf("Processing file: %s\n", filepath.Base(imageData.Source))
+	}
+
+	_, err = decoder.Token()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", dataHandler)
-	http.HandleFunc("/stop", func(rw http.ResponseWriter, req *http.Request) { os.Exit(0) })
-	log.Println("Server started")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	processImagesFromJSON(os.Stdin, 10)
 }
